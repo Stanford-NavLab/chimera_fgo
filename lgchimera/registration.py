76a73e69643cf0d12513eb2d4de423ea2c9ff056
Adam Dai <adamdai97@gmail.com>
2022-11-11 11:49:45 -0800

    lidar odometry with open3d point-to-plane ICP

--- lgchimera/registration.py
+++ lgchimera/registration.py
@@ -5,9 +5,27 @@ Functions and utilities for point-cloud registration.
 """
 
 import open3d as o3d
+import time
 
 
-def initalize_source_and_target(source_data, target_data):
+def initialize_source_and_target(source_data, target_data):
+    """Initialize source and target
+    
+    Parameters
+    ----------
+    source_data : np.array (N x 3)
+        Source point cloud array
+    target_data : np.array (N x 3)
+        Target point cloud array
+    
+    Returns
+    -------
+    source : PointCloud
+        Source as Open3D point cloud
+    target : PointCloud
+        Target as Open3D point cloud
+
+    """
     source = o3d.geometry.PointCloud()
     source.points = o3d.utility.Vector3dVector(source_data)
     source.estimate_normals()
@@ -21,9 +39,31 @@ def initalize_source_and_target(source_data, target_data):
     return source, target
 
 def p2p_ICP(source, target, threshold, trans_init):
+    """Point-to-Point ICP
+    
+    Parameters
+    ----------
+    
+    
+    """
     start_time = time.time()
     reg_p2p = o3d.pipelines.registration.registration_icp(
         source, target, threshold, trans_init,
         o3d.pipelines.registration.TransformationEstimationPointToPoint())
+    eval_time = time.time() - start_time
+    return reg_p2p, eval_time
+
+
+def p2pl_ICP(source, target, threshold, trans_init):
+    """Point-to-Plane ICP
+    
+    Parameters
+    ----------
+    
+    """
+    start_time = time.time()
+    reg_p2p = o3d.pipelines.registration.registration_icp(
+        source, target, threshold, trans_init,
+        o3d.pipelines.registration.TransformationEstimationPointToPlane())
     eval_time = time.time() - start_time
     return reg_p2p, eval_time
