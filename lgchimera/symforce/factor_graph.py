df725faa298d1ee65be9ef7501045c4750550518
Adam Dai <adamdai97@gmail.com>
2023-01-14 08:41:43 -0800

    evaluation script

--- lgchimera/symforce/factor_graph.py
+++ lgchimera/symforce/factor_graph.py
@@ -54,6 +54,30 @@ def range_residual(
 def position_residual(
         pose: sf.Pose3, 
         m_pos: sf.V3, 
+        sigma: sf.V3) -> sf.V3:
+    """Position measurement residual
+
+    Parameters
+    ----------
+    pose : sf.Pose3
+        Robot pose
+    m_pos : sf.V3
+        Position measurement
+    sigma : sf.V3
+        Diagonal covariance of measurements
+    
+    Returns
+    -------
+    sf.V3
+        Residual
+
+    """
+    return sf.M.diag(sigma.to_flat_list()).inv() * sf.V3(pose.t - m_pos)
+
+
+def orientation_residual(
+        pose: sf.Pose3, 
+        m_ori: sf.Rot3, 
         sigma: sf.V3, 
         epsilon: sf.Scalar) -> sf.V3:
     """Position measurement residual
@@ -73,7 +97,8 @@ def position_residual(
         Residual
 
     """
-    return sf.V3(pose.t - m_pos) / sigma
+    tangent_error = pose.R.local_coordinates(m_ori, epsilon=epsilon)
+    return sf.M.diag(sigma.to_flat_list()).inv() * sf.V3(tangent_error) 
 
 
 def odometry_residual(
@@ -162,7 +187,80 @@ def build_factors(num_poses, num_satellites, gps_rate=1, fix_first_pose=False):
         )
 
 
-def fgo(init_pos, sat_pos, m_ranges, odom, odom_sigma, range_sigma, gps_rate, fix_first_pose=False, debug=False):
+def build_values_att(init_poses, satpos, m_ranges, m_odometry, m_attitudes, range_sigma, odom_sigma, att_sigma):
+    """Build values for factor graph
+
+    Parameters
+    ----------
+    num_poses : int
+        Number of poses 
+    satpos : np.ndarray
+        Satellite positions
+    m_ranges : np.ndarray
+        Measured ranges
+    m_odometry : list of tuple (R, t)
+        Measured odometry
+
+    """
+    values = Values()
+
+    values["poses"] = init_poses
+    values["satellites"] = [sf.V3(pos) for pos in satpos]
+
+    values["ranges"] = m_ranges.tolist()
+    values["odometry"] = [sf.Pose3(R = sf.Rot3.from_rotation_matrix(R), t = sf.V3(t)) for R, t in m_odometry]
+    values["attitudes"] = [sf.Rot3.from_rotation_matrix(R) for R in m_attitudes]
+
+    values["range_sigma"] = range_sigma
+    values["odom_sigma"] = odom_sigma
+    values["att_sigma"] = att_sigma
+
+    values["epsilon"] = sf.numeric_epsilon
+
+    return values
+
+
+def build_factors_att(num_poses, num_satellites, gps_rate=1, fix_first_pose=False):
+    """Build range and odometry factors
+
+    """
+    start_idx = gps_rate if fix_first_pose else 0
+    for i in range(start_idx, num_poses, gps_rate):
+        for j in range(num_satellites):
+            yield Factor(
+                residual=range_residual,
+                keys=[
+                    f"poses[{i}]",
+                    f"satellites[{j}]",
+                    f"ranges[{i}][{j}]",
+                    "range_sigma",
+                    "epsilon",
+                ],
+            )
+        yield Factor(
+                residual=orientation_residual,
+                keys=[
+                    f"poses[{i}]",
+                    f"attitudes[{i}]",
+                    "att_sigma",
+                    "epsilon",
+                ],
+            )
+
+    for i in range(num_poses - 1):
+        yield Factor(
+            residual=odometry_residual,
+            keys=[
+                f"poses[{i}]",
+                f"poses[{i + 1}]",
+                f"odometry[{i}]",
+                "odom_sigma",
+                "epsilon",
+            ],
+        )
+
+
+def fgo(init_pos, sat_pos, m_ranges, m_odom, range_sigma, odom_sigma, gps_rate, fix_first_pose=False, debug=False):
     """Factor graph optimization
 
     Parameters
@@ -173,9 +271,47 @@ def fgo(init_pos, sat_pos, m_ranges, odom, odom_sigma, range_sigma, gps_rate, fi
     N_SATS = len(sat_pos)
 
     # Build values and factors
-    values = build_values(init_pos, sat_pos, m_ranges, odom, 
+    values = build_values(init_pos, sat_pos, m_ranges, m_odom, 
                         range_sigma, odom_sigma)
     factors = build_factors(N_POSES, N_SATS, gps_rate=gps_rate, fix_first_pose=fix_first_pose)
+
+    # Select the keys to optimize - the rest will be held constant
+    start_idx = 1 if fix_first_pose else 0
+    optimized_keys = [f"poses[{i}]" for i in range(start_idx, N_POSES)]
+
+    # Create the optimizer
+    optimizer = Optimizer(
+        factors=factors,
+        optimized_keys=optimized_keys,
+        # Return problem stats for every iteration
+        debug_stats=debug,
+        # Customize optimizer behavior
+        params=Optimizer.Params(verbose=debug, initial_lambda=1e4, lambda_down_factor=0.5),
+    )
+
+    # Solve and return the result
+    result = optimizer.optimize(values) 
+    return result
+
+
+def fgo_att(init_pos, sat_pos, m_ranges, m_odom, m_att, range_sigma, odom_sigma, att_sigma, gps_rate, fix_first_pose=False, debug=False):
+    """Factor graph optimization
+
+    Parameters
+    ----------
+
+    """
+    print("attitude optimization")
+    N_POSES = len(init_pos)
+    N_SATS = len(sat_pos)
+
+    # Build values and factors
+    # values = build_values(init_pos, sat_pos, m_ranges, m_odom, 
+    #                     range_sigma, odom_sigma)
+    # factors = build_factors(N_POSES, N_SATS, gps_rate=gps_rate, fix_first_pose=fix_first_pose)
+    values = build_values_att(init_pos, sat_pos, m_ranges, m_odom, m_att,
+                        range_sigma, odom_sigma, att_sigma)
+    factors = build_factors_att(N_POSES, N_SATS, gps_rate=gps_rate, fix_first_pose=fix_first_pose)
 
     # Select the keys to optimize - the rest will be held constant
     start_idx = 1 if fix_first_pose else 0
---
+++ scripts/monte_carlo.py
@@ -0,0 +1,182 @@
+import numpy as np
+import struct 
+import os
+import plotly.graph_objects as go
+import time
+from scipy.stats import chi2
+from tqdm import tqdm
+
+import lgchimera.general as general
+from lgchimera.io import read_lidar_bin, read_gt
+from lgchimera.kitti_util import process_kitti_gt, load_icp_results
+from lgchimera.geom_util import euler_to_R
+
+from lgchimera.symforce.factor_graph import fgo
+
+import symforce
+try:
+    symforce.set_epsilon_to_symbol()
+except symforce.AlreadyUsedEpsilon:
+    print("Already set symforce epsilon")
+    pass 
+
+import symforce.symbolic as sf
+
+# ================================================= #
+#                    PARAMETERS                     #
+# ================================================= #
+
+kitti_seq = '0027'
+start_idx = 0
+
+N_SHIFT = 10
+N_WINDOW = 100
+GPS_RATE = 10
+
+TRAJLEN = 2000   # 4544
+
+MAX_BIAS = 50  # [m]
+ATTACK_START = TRAJLEN // 2
+
+ATTITUDE = False
+
+# Lidar odometry covariance
+ODOM_R_SIGMA = 0.01  # 
+ODOM_T_SIGMA = 0.05  # [m]
+#ODOM_SIGMA = np.array([0.05, 0.05, 0.05, 0.2, 0.2, 0.2])
+ODOM_SIGMA = np.ones(6)
+ODOM_SIGMA[:3] *= ODOM_R_SIGMA
+ODOM_SIGMA[3:] *= ODOM_T_SIGMA
+
+# ================================================= #
+#                       SETUP                       #
+# ================================================= #
+
+# Load the data
+print("Loading data...")
+gtpath = os.path.join(os.getcwd(), '..', 'data', 'kitti', kitti_seq, 'oxts', 'data')
+gt_enu, gt_Rs, gt_attitudes = process_kitti_gt(gtpath, start_idx=start_idx)
+
+data_path = os.path.join(os.getcwd(), '..', 'data', 'kitti', kitti_seq, 'results', 'p2pl_icp')
+lidar_Rs, lidar_ts, positions, lidar_covariances = load_icp_results(data_path, start_idx=start_idx)
+
+# Satellite positions
+# Uniformly arranged in a circle of radius R at constant altitude
+R = 1e4
+SAT_ALT = 1e4
+N_SATS = 10
+satpos_enu = np.zeros((N_SATS, 3))
+satpos_enu[:,0] = np.cos(np.linspace(0, 2*np.pi, N_SATS, endpoint=False)) * R
+satpos_enu[:,1] = np.sin(np.linspace(0, 2*np.pi, N_SATS, endpoint=False)) * R
+satpos_enu[:,2] = SAT_ALT
+
+# Compute threshold
+alpha = 0.001  # False alarm (FA) rate
+T = chi2.ppf(1-alpha, df=N_SATS*(N_WINDOW/GPS_RATE))
+
+# Lidar odometry
+lidar_odom = [None] * TRAJLEN
+for i in range(TRAJLEN - 1):
+    lidar_odom[i] = (lidar_Rs[i], lidar_ts[i])
+
+# Spoofed trajectory
+spoofed_pos = gt_enu[:TRAJLEN].copy()
+
+gps_spoofing_biases = np.zeros(TRAJLEN)  
+gps_spoofing_biases[ATTACK_START:] = np.linspace(0, MAX_BIAS, TRAJLEN-ATTACK_START)  # Ramping attack
+
+spoofed_pos[:,0] += gps_spoofing_biases
+
+# Spoofed ranges
+PR_SIGMA = 6
+spoofed_ranges = np.zeros((TRAJLEN, N_SATS))
+for i in range(TRAJLEN):
+    for j in range(N_SATS):
+        spoofed_ranges[i,j] = np.linalg.norm(spoofed_pos[i] - satpos_enu[j]) + np.random.normal(0, PR_SIGMA)
+
+N_RUNS = 100
+
+# ================================================= #
+#                     RUN FGO                       #
+# ================================================= #
+
+for i in range(N_RUNS):
+    print("Run", i)
+    # Regenerate ranges
+    spoofed_ranges = np.zeros((TRAJLEN, N_SATS))
+    for i in range(TRAJLEN):
+        for j in range(N_SATS):
+            spoofed_ranges[i,j] = np.linalg.norm(spoofed_pos[i] - satpos_enu[j]) + np.random.normal(0, PR_SIGMA)
+
+    graph_positions = np.zeros((TRAJLEN, 3))
+    init_poses = [sf.Pose3.identity()] * N_WINDOW
+    init_poses[0] = sf.Pose3(sf.Rot3.from_rotation_matrix(gt_Rs[0]), sf.V3(gt_enu[0]))
+    e_ranges = np.zeros((N_WINDOW//GPS_RATE, N_SATS))
+    qs = np.zeros(TRAJLEN//GPS_RATE)
+
+    for k in tqdm(range((TRAJLEN - N_WINDOW) // N_SHIFT)):
+        #print(N_SHIFT * k, '/', TRAJLEN)
+        window = slice(N_SHIFT * k, N_SHIFT * k + N_WINDOW)
+        odom = lidar_odom[window]
+        ranges = spoofed_ranges[window]
+
+        result = fgo(init_poses, satpos_enu, ranges, odom, PR_SIGMA, ODOM_SIGMA, GPS_RATE, fix_first_pose=True, debug=False)
+
+        # Extract optimized positions
+        window_positions = np.zeros((N_WINDOW, 3))
+        for i in range(N_WINDOW):
+            pose = result.optimized_values["poses"][i]
+            window_positions[i] = pose.position().flatten()   
+
+        # Save trajectory
+        graph_positions[N_SHIFT*k:N_SHIFT*(k+1)] = window_positions[:N_SHIFT]
+
+        # Update initial poses
+        init_poses[:-N_SHIFT] = result.optimized_values["poses"][N_SHIFT:]
+
+        # Use lidar odometry to initialize latest new poses
+        n_pose = result.optimized_values["poses"][-1]
+        n_pose = sf.Pose3(sf.Rot3.from_rotation_matrix(n_pose.R.to_rotation_matrix()), sf.V3(n_pose.t))
+        for i in range(N_SHIFT):
+            lidar_T = sf.Pose3(sf.Rot3.from_rotation_matrix(lidar_Rs[N_SHIFT*k + N_WINDOW + i]), sf.V3(lidar_ts[N_SHIFT*k + N_WINDOW + i]))
+            n_pose = lidar_T * n_pose
+            init_poses[-N_SHIFT+i] = n_pose
+
+        # Compute test statistic
+        for i in range(N_WINDOW//GPS_RATE):
+            for j in range(N_SATS):
+                e_ranges[i,j] = np.linalg.norm(window_positions[::GPS_RATE][i] - satpos_enu[j])
+        q = np.sum((ranges[::GPS_RATE] - e_ranges)**2) / PR_SIGMA**2
+        #print("q = ", q)
+        qs[k] = q
+
+    OPT_TRAJLEN = N_SHIFT*(k+1)
+
+    graph_positions = graph_positions[:OPT_TRAJLEN]
+    gt_enu = gt_enu[:OPT_TRAJLEN]
+    qs = qs[:OPT_TRAJLEN]
+
+    # Compute statistics
+    # RMSE error
+    rmse_xyz = np.sqrt(np.mean((graph_positions - gt_enu)**2, axis=0))
+    print("RMSE (xyz): ", rmse_xyz)
+    print("RMSE (overall): ", rmse_xyz.mean())
+
+    # Detection / time to detection
+    print('Max test statistic: ', qs.max())
+    attack_detected = np.any(qs > T)
+    if attack_detected:
+        t_detected = np.argmax(qs > T) * N_SHIFT
+        print("Attack detected at t = ", t_detected)
+
+    # Max errors
+    max_xyz = np.max(np.abs(graph_positions - gt_enu), axis=0)
+    print("Max error (xyz): ", max_xyz)
+
+    # Mean errors
+
+
+    # Plots
+    # Trajectory
+    
+    # 
